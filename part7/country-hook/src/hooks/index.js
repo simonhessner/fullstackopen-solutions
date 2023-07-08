9f816318e0bdc769695dfadd74d1772daa140791
Simon <simon@hessner.de>
2023-07-08 20:15:33 +0100

    Change default country found: false

--- part7/country-hook/src/hooks/index.js
+++ part7/country-hook/src/hooks/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import axios from 'axios'
 
 export const useCountry = (country) => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState({ found: false })
 
   useEffect(() => {
     axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`).then(result => {
