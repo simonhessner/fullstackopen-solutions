6815789f55ed4b3cdec95af44f8c7cb2e8b0820a
Simon <simon@hessner.de>
2023-07-08 19:45:36 +0100

    Make spread syntax easier to use

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -58,9 +58,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const {reset: resetContent, ...content} = useField('text')
-  const {reset: resetAuthor, ...author} = useField('text')
-  const {reset: resetInfo, ...info} = useField('text')
+  const [content, resetContent] = useField('text')
+  const [author, resetAuthor] = useField('text')
+  const [info, resetInfo] = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -9,10 +9,12 @@ export const useField = (type) => {
 
   const reset = () => setValue('')
 
-  return {
-    type,
-    value,
-    onChange,
+  return [
+    {
+      type,
+      value,
+      onChange
+    },
     reset
-  }
+  ]
 }
